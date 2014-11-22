4d45721be620be3fcfefd95b9332adee386954c0
Manu <servais.manuel@gmail.com>
2014-11-22 20:45:48 +0100

    add deployer

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+
+export SYMFONY_ENV=prod
+composer install --no-dev --optimize-autoloader
+app/console cache:clear --env=prod --no-debug
+app/console assetic:dump --env=prod --no-debug
+app/console assets:install --symlink --env=prod --no-debug
+# app/console  doctrine:schema:drop --force --env=prod --no-debug
+app/console  doctrine:schema:update --env=prod --no-debug
+# app/console  doctrine:fixtures:load --env=prod --no-debug
