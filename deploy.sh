5bc4d75c5cc37f16364cf4dade517af3f1547f87
Manu <servais.manuel@gmail.com>
2014-11-22 20:59:29 +0100

    update deployer

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!bin/bash
 
 export SYMFONY_ENV=prod
+git pull
 composer install --no-dev --optimize-autoloader
 app/console cache:clear --env=prod --no-debug
 app/console assetic:dump --env=prod --no-debug
