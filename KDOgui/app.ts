a0d18063e56cb904e8c1dbb24c622717b01ef0dd
movrack <servais.manuel@gmail.com>
2015-08-31 19:31:04 +0200

    wip

--- KDOgui/app.ts
+++ KDOgui/app.ts
@@ -1,20 +1,6 @@
-/// <reference path="typings/angular2/angular2.d.ts" />
-import {Component, View, bootstrap} from 'angular2/angular2';
-// Annotation section
-@Component({ selector: 'app' })
-@View(
-    {
-        templateUrl: 'views/app.html',
-        directives: [ChildComponent]
-    }
-)
+import greeter = require('./greeter');
+import $ = require('jquery');
 
-// Component controller
-class App {
-    name: string;
-    constructor() {
-        this.name = 'Alice';
-    }
-}
-
-bootstrap(App);
+$(() => {
+  $(document.body).html(greeter("World"));
+});
